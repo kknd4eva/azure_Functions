ab7045017ab58e593e2440f4d589073b4dcd8dd9
James Matson <james.matson@api.net.au>
2022-12-03 23:27:21 +1100

    Added steps to bootstrap database

--- HttpTriggerGetSqlData.cs
+++ HttpTriggerGetSqlData.cs
@@ -6,7 +6,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace CloudySkies.Function
 {
@@ -14,22 +14,14 @@ namespace CloudySkies.Function
     {
         [FunctionName("HttpTriggerGetSqlData")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "employees")] HttpRequest req,
+            ILogger log,
+            [Sql("select * from Employees",
+            CommandType = System.Data.CommandType.Text,
+            ConnectionStringSetting = "SqlConnectionString")]
+            IEnumerable<Employee> employee)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(employee);
         }
     }
 }
--- README.md
+++ README.md
@@ -10,7 +10,14 @@ A small starter project showing the use of the following technologies
 (The resource group bicep file is deployed at the subscription level, then it triggers the creation of the SQL server and the Azure Function App in the scope of the created resource group)
 
 ```
-az deployment sub create --location eastus --template-file ./infrastructure/resource_group.bicep
+KKnD@LAPTOP-M1FLIIDM MINGW64 /d/repos/azure_Functions (master)
+$ az deployment sub create --location eastus --template-file ./infrastructure/resource_group.bicep
+```
+
+### Use sqlcmd to bootstrap the created database with a table and some rows of data
+```
+KKnD@LAPTOP-M1FLIIDM MINGW64 /d/repos/azure_Functions (master)
+$ sqlcmd -S tcp:xxxxxxxxx.database.windows.net -d azurefunctiondb -U dbmaster -P xxxxxxxx -i ./sql_script/setup.sql
 ```
 
 ### To deploy the function
