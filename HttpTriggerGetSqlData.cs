79991d17c0f28ee969231e646770cea89fdb24da
James Matson <James.Matson@api.net.au>
2022-12-04 13:52:35 +1100

    Final readme updates, changes to sql schema, function code.

--- HttpTriggerGetSqlData.cs
+++ HttpTriggerGetSqlData.cs
@@ -7,21 +7,34 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;  
 
 namespace CloudySkies.Function
 {
     public static class HttpTriggerGetSqlData
     {
+        public class SalesTransaction
+        {
+            public int Id { get; set; }
+            public DateTime sale_date { get; set; }
+            public string customer_id {get; set;}
+            public string product_id { get; set; }
+            public int quantity { get; set; }
+            public decimal price { get; set; }
+        }
+
         [FunctionName("HttpTriggerGetSqlData")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "employees")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "transactions")] HttpRequest req,
             ILogger log,
-            [Sql("select * from Employees",
+            [Sql("select * from sales_transaction",
             CommandType = System.Data.CommandType.Text,
             ConnectionStringSetting = "SqlConnectionString")]
-            IEnumerable<Employee> employee)
+            IEnumerable<SalesTransaction> salesTransactions)
         {
-            return new OkObjectResult(employee);
+            log.LogInformation("Query received for sales transactions...");
+            
+            return new OkObjectResult(salesTransactions);
         }
     }
 }
--- sql_script/setup.sql
+++ sql_script/setup.sql
@@ -1,5 +1,6 @@
 CREATE TABLE sales_transaction (
     id INT NOT NULL IDENTITY(1,1),
+    sale_date DATETIME,
     customer_id INT NOT NULL,
     product_id INT NOT NULL,
     quantity INT NOT NULL,
@@ -7,14 +8,14 @@ CREATE TABLE sales_transaction (
     PRIMARY KEY (id)
 );
 GO
-INSERT INTO sales_transaction (customer_id, product_id, quantity, price)
-VALUES (1, 1, 1, 10.00),
-       (1, 2, 1, 20.00),
-       (1, 3, 1, 30.00),
-       (2, 1, 1, 10.00),
-       (2, 2, 1, 20.00),
-       (2, 3, 1, 30.00),
-       (3, 1, 1, 10.00),
-       (3, 2, 1, 20.00),
-       (3, 3, 1, 30.00);
+INSERT INTO sales_transaction (sale_date, customer_id, product_id, quantity, price)
+VALUES (GETDATE(), 1, 1, 1, 10.00),
+         (GETDATE(), 1, 2, 1, 20.00),
+         (GETDATE(), 1, 3, 1, 30.00),
+         (GETDATE(), 2, 1, 1, 10.00),
+         (GETDATE(), 2, 2, 1, 20.00),
+         (GETDATE(), 2, 3, 1, 30.00),
+         (GETDATE(), 3, 1, 1, 10.00),
+         (GETDATE(), 3, 2, 1, 20.00),
+         (GETDATE(), 3, 3, 1, 30.00);
 GO
